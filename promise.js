c9a02d70203c9759e4c896d6339071aa626a8473
zakaria29 <zakaria@smktelkom-mlg.sch.id>
2020-10-06 10:33:24 +0700

    update promise

--- promise.js
+++ promise.js
@@ -9,6 +9,15 @@ penjumlahan = (a,b) => {
     })
 }
 
+sqrt = (a) => {
+    return new Promise((resolve, reject) => {
+        if (typeof Math.sqrt(a) == "number") {
+            resolve(Math.sqrt(a))
+        }
+        reject(new Error(`not squared number`))
+    })
+}
+
 // console.log(`command 1`);
 // console.log(`command 2`);
 // penjumlahan(3,"lima")
@@ -22,9 +31,28 @@ main = async() => {
         console.log(`Hasil penjumlahan: ${hasil}`);
         let result = await penjumlahan(hasil, 50)
         console.log(`Hasil akhir: ${result}`);
+
+        let s = await sqrt(90)
+        console.log(`Hasil akar kuadrat: ${s}`);
     } catch (error) {
         console.log(error.message);
     }
 }
 
 main()
+
+
+/**
+ * case study
+ * 1. buat promise utk menghitung usia berdasarkan parameter thn kelahiran,
+ *  (akan error jika parameter thn diatas tahun saat ini), pastikan juga parameter bertipe numeric
+ *  exp: countUsia(1990) -> 30
+ *       countUsia(2030) -> error
+ * 
+ * 2. buat promise utk menghitung nilai faktorial dari parameter sebuah angka,
+ *  pastikan nilai parameter bilangan bulat positif
+ *  exp: faktorial(5) -> 120
+ *       faktorial(-3) -> error
+ *       faktorial(2.5) -> error
+ * 
+ */
